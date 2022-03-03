676ee9a941076f622291fe4452bbbb002915b55f
JuanPablo Andrew Heredia <jherediaparillo@gmail.com>
2022-03-03 11:32:32 -0500

    v2

--- pp3p.py
+++ pp3p.py
@@ -1,31 +1,61 @@
 # testing use of python 2 and 3
 # python 3.4 here
 
-import subprocess
+#import subprocess
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
+import os
+import time
+from datetime import date
+
+from multimodalDLforER.processor import Processor
+from argparse import Namespace
+
+today = date.today().strftime("%b-%d-%Y")
+
+args = Namespace(configuration='multimodalDLforER/configs/embracenet_plus.json',
+				cuda=0, dataset=None, inputfile='siu.jpg',
+				modality=None, mode='inference',
+				multimodel='multimodalDLforER/checkpoints/checkpoints/ebnplus/allmerge_ebnp.pth',
+				oversample=False, pretrained=True, savename=None,
+				threshold='multimodalDLforER/checkpoints/thresholds/thresholds_validation.npy',
+				unimodal=False, unimodel=None, unimodels='multimodalDLforER/checkpoints/models/')
+P = Processor(args)
 
 def process_image(image, emotion=True, size=(224,224)):
-	image = cv2.resize(image, size)
-	print(image.shape)
+	#image = cv2.resize(image, size)
+	P.Inputfile = image
+	print('a')
+	P.start()
+	print('b')
+	#print(image.shape)
 	# image = np.transpose(image,(2,0,1))
-	if emotion:
-		prediction = 0
-		return (prediction, image)
-	return image
+	with open('results'+image[-4]) as f:
+		lines = f.readlines()
+		print(lines)
 
 if __name__ == '__main__':
 
-	script = ["python2.7", "pp2p.py"]    
-	process = subprocess.Popen(" ".join(script), shell=True, env={"PYTHONPATH": "."})
-	
+	#script = ["python2", "pp2p.py"]    
+	#process = subprocess.Popen(" ".join(script), shell=True)#, env={"PYTHONPATH": "."})
+	count = 0
+
 	while True:
+		os.system('python2 pp2p.py --name '+today +'_'+str(count))
 		try:
-			img = cv2.imread('camImage.png')
-			break
+			#img = cv2.imread('captures/'+today +'_'+ str(count)+'.png')
+			#print(img.shape)
+			process_image('captures/'+today +'_'+ str(count)+'.png')
+			count += 1
+			time.sleep(5)
 		except:
-			print('no image yet')
+			print('error')
+			break
+		if count==11:
+			break
 	
-	print(img.shape)
+	#print(img.shape)
+	#plt.imshow(img)
+	#plt.show()
 	
